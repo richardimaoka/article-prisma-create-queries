ad159f9a49f714c76376bb1993de69580a941b9e
richardimaoka <richard.s.imaoka@gmail.com>
2025-11-07 03:05:18 +0900

    update prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig, env } from "prisma/config";
 
+type Env = {
+  DATABASE_URL: string;
+};
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
@@ -7,6 +11,6 @@ export default defineConfig({
   },
   engine: "classic",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env<Env>("DATABASE_URL"),
   },
 });
