992481ddbdc01dfd1f96ec87f20b334be6fd00bf
richardimaoka <richard.s.imaoka@gmail.com>
2025-11-07 08:35:37 +0900

    one-to-many-init

---
+++ one-to-many-relation/src/create_nested_connectOrCreate.ts
@@ -0,0 +1,75 @@
+import "dotenv/config";
+import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../generated/prisma/client";
+
+const dbPath = process.env.PRISMA_CLIENT_DATABASE_URL;
+if (!dbPath) {
+  throw new Error("Environment variable SSDATABASE_URL not defined!!");
+}
+
+const adapter = new PrismaBetterSQLite3({ url: dbPath });
+const prisma = new PrismaClient({ adapter });
+
+// The creation order MUST be:
+//   OK!! 1) user -> 2) bio(connect user)
+// The other way around is impossible:
+//   BAD! 1) bio  -> 2) user(connect bio)
+// because the bio needs userId as in schema.prisma
+async function main() {
+  // As described above, it is impossible to create bio ONLY in advance.
+  //   const bio = await prisma.bio.create({
+  //     // TypeScript Error: Property 'user' is missing in type '{ text: string; }'
+  //     //                   but required in type 'BioCreateInput'.ts(2322)
+  //     data: {
+  //       text: "Alice's bio",
+  //       // userId or user is required
+  //     },
+  //   });
+
+  const user = await prisma.user.create({
+    data: {
+      name: "Alice",
+    },
+  });
+
+  console.log("created user:", user);
+
+  const bio = await prisma.bio.create({
+    data: {
+      text: "Alice's Bio",
+      user: {
+        connectOrCreate: {
+          where: {
+            id: user.id,
+          },
+          create: {
+            name: "Alice",
+          },
+        },
+      },
+    },
+  });
+
+  console.log("created bio:", bio);
+
+  const foundUser = await prisma.user.findUnique({
+    include: {
+      bio: true,
+    },
+    where: {
+      id: user.id,
+    },
+  });
+
+  console.log("found user:", foundUser);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
